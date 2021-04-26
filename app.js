8132e01c197f3375986274a157e986fea0b05a04
Shalaw Fatah <shalaw.fatah@gmail.com>
2021-04-26 09:48:53 -0700

    added comments for the object

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// This is an Object
 const westeros = {
     cersi: "Lannister",
     arya: "Stark",
@@ -11,9 +12,16 @@ const westeros = {
     samwell: "Tarley",
     ramsay: "Bolton"
 }
+
+// TO FIND ALL THE KEYS OF AN OBJECT
 let allKeys = Object.keys(westeros)
 console.log(allKeys)
+
+// TO FIND ALL THE VALUES OF AN OBJECT
 let allVals = Object.values(westeros)
 console.log('Values: ', allVals)
+
+// TO FIND EACH SINGLE KEY-VALUE PAIR OF AN OBJECT
 let allentries = Object.entries(westeros)
 console.log(allentries)
+
