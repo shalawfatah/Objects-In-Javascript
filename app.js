b2ae37940a21a4d3975e77a7f21318a36a97b402
Shalaw Fatah <shalaw.fatah@gmail.com>
2021-04-26 09:44:09 -0700

    objects and their methods

---
+++ app.js
@@ -0,0 +1,15 @@
+const westeros = {
+    cersi: "Lannister",
+    arya: "Stark",
+    jon: "Snow",
+    brinne: "Tarth",
+    daenerys: "Targaryen",
+    theon: "Greyjoy",
+    margaery: "Tyrell",
+    sandor: "Clegane",
+    sandor: "Clegane",
+    samwell: "Tarley",
+    ramsay: "Bolton"
+}
+let allKeys = Object.keys(westeros)
+console.log(allKeys)
